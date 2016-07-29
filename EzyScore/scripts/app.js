ada6ce63a91d504078d47ac97cc9b34c391f58da
Tawfiq Khan <tawfiq.khan@itaas.dimensiondata.com>
2016-07-29 16:27:54 +1000

    Add the files

---
+++ EzyScore/scripts/app.js
@@ -0,0 +1,1 @@
+﻿var ezyScoreApp = angular.module('ezyScoreApp', []);
---
+++ EzyScore/scripts/controllers/scoreController.js
@@ -0,0 +1,7 @@
+﻿ezyScoreApp.controller('scoreController', ['$scope', '$http', function ($scope, $http) {
+
+    $scope.overs = 10;
+    $scope.wickets = 5;
+    $scope.status = 'new';
+
+}]);
